90b5153420cf9ecfd3b32e8c9b5483272ba498dd
Alex <alexwolf47@gmail.com>
2019-01-26 16:54:26 +0000

    adding robot solution

---
+++ robot-simulator/robot_simulator.exs
@@ -0,0 +1,124 @@
+defmodule RobotSimulator do
+  @valid_directions [:north, :east, :south, :west]
+  @invalid_direction {:error, "invalid direction"}
+  @invalid_position {:error, "invalid position"}
+
+  # def create() do
+  #   robot = %{"direction" => :north, "position" => {0, 0}}
+  # end
+
+  @doc """
+  Create a Robot Simulator given an initial direction and position.
+
+  Valid directions are: `:north`, `:east`, `:south`, `:west`
+  """
+  @spec create(direction :: atom, position :: {integer, integer}) :: any
+  def create(direction \\ nil, position \\ nil) do
+    robot =
+      case {direction, position} do
+        {nil, nil} ->
+          %{"direction" => :north, "position" => {0, 0}}
+
+        {_, _} ->
+          %{"direction" => direction, "position" => position}
+      end
+
+    robot |> check_valid_direction() |> check_valid_position()
+  end
+
+  defp check_valid_direction(robot) do
+    if Enum.member?(@valid_directions, robot["direction"]) do
+      robot
+    else
+      @invalid_direction
+    end
+  end
+
+  defp check_valid_position({:error, _} = robot) do
+    robot
+  end
+
+  defp check_valid_position(robot) do
+    position = robot["position"]
+
+    if is_tuple(position) && tuple_size(position) == 2 && is_integer(elem(position, 0)) &&
+         is_integer(elem(position, 1)) do
+      robot
+    else
+      @invalid_position
+    end
+  end
+
+  @doc """
+  Simulate the robot's movement given a string of instructions.
+
+  Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
+  """
+  @spec simulate(robot :: any, instructions :: String.t()) :: any
+  def simulate({:error, _}, _) do
+    {:error, "invalid instruction"}
+  end
+
+  def simulate(robot, instructions) when is_binary(instructions) do
+    instructions = to_charlist(instructions)
+    simulate(robot, instructions)
+  end
+
+  def simulate(robot, [this_move | later_moves]) do
+    moving_robot = move(robot, this_move)
+    simulate(moving_robot, later_moves)
+  end
+
+  def simulate(finished_moving_robot, []) do
+    finished_moving_robot
+  end
+
+  def move(robot, ?R) do
+    case robot["direction"] do
+      :north -> %{robot | "direction" => :east}
+      :east -> %{robot | "direction" => :south}
+      :south -> %{robot | "direction" => :west}
+      :west -> %{robot | "direction" => :north}
+    end
+  end
+
+  def move(robot, ?L) do
+    case robot["direction"] do
+      :north -> %{robot | "direction" => :west}
+      :east -> %{robot | "direction" => :north}
+      :south -> %{robot | "direction" => :east}
+      :west -> %{robot | "direction" => :south}
+    end
+  end
+
+  def move(robot, ?A) do
+    case robot["direction"] do
+      :north -> Map.update(robot, "position", {0, 0}, fn {x, y} -> {x, y + 1} end)
+      :east -> Map.update(robot, "position", {0, 0}, fn {x, y} -> {x + 1, y} end)
+      :south -> Map.update(robot, "position", {0, 0}, fn {x, y} -> {x, y - 1} end)
+      :west -> Map.update(robot, "position", {0, 0}, fn {x, y} -> {x - 1, y} end)
+    end
+  end
+
+  def move(_robot, _) do
+    {:error, "invalid instruction"}
+  end
+
+  # @doc """
+  # Return the robot's direction.
+
+  # Valid directions are: `:north`, `:east`, `:south`, `:west`
+  # """
+  @spec direction(robot :: any) :: atom
+  def direction(robot) do
+    robot["direction"]
+  end
+
+  @doc """
+  Return the robot's position.
+  """
+  @spec position(robot :: any) :: {integer, integer}
+  def position(robot) do
+    robot["position"]
+  end
+end
